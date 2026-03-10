d925765f6c06b1bb83a263bb073f38cddc3c7c03
sumitdoescode <sumit.does.code@gmail.com>
2026-03-11 02:42:37 +0530

    let's see

--- app/api/healthcheck/route.ts
+++ app/api/healthcheck/route.ts
@@ -1,5 +1,9 @@
 import { NextResponse } from "next/server";
 
+export const config = {
+  regions: ["bom1"]
+};
+
 export async function GET() {
     return NextResponse.json({ message: "OK", timestamp: new Date().toISOString(), uptime: process.uptime() });
 }
